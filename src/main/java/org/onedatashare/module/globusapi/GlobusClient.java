b08480dc1470b72e14244507e0ed7906810ae7f1
yinyifu <yifuyin@buffalo.com>
2019-01-31 12:06:09 -0500

    Edited requests to successfully retrieve accessToken and also patched security problem

--- src/main/java/org/onedatashare/module/globusapi/GlobusClient.java
+++ src/main/java/org/onedatashare/module/globusapi/GlobusClient.java
@@ -5,39 +5,47 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import sun.misc.BASE64Encoder;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+//@Service
 public class GlobusClient {
     private static final String CONTENT_TYPE = "application/json";
     private final WebClient webClient;
-    @Value("${auth.base.url}")
-    String AUTH_BASE_URL;// = "https://auth.globus.org/v2/oauth2";
-    @Value("$(auth.uri")
-    String AUTH_URI;// = "/authorize";
-    @Value("${token.uri}")
-    String TOKEN_URI;// = "/token";
-    @Value("${transfer.base.url}")
-    String TRANSFER_BASE_URL;// = "https://transfer.api.globusonline.org/v0.10";
-    @Value("$(transfer.uri)")
-    String TRANSFER_URI;// = "/transfer";
-    @Value("${submission.uri}")
-    String SUBMISSION_URI;// = "/submission_id";
-    @Value("${redirect.uri}")
-    String REDIRECT_URI;// = "https://127.0.0.1:8080/api/stork/oauth";
-    @Value("${client.id}")
-    String CLIENT_ID;// = "6843af68-87f8-4341-bfc4-3db4b1e2d845";
-    @Value("${client.secret}")
-    String CLIENT_SECRET;// = "IsqQ6AlvDCi/z5em+D6NJl2cRFq9gFt0jm9GRseQek0=";
-    @Value("${scope}")
-    String SCOPE;// = "urn:globus:auth:scope:transfer.api.globus.org:all urn:globus:auth:scope:auth.globus.org:view_identities offline_access";
-    @Value("$(response.type)")
-    String RESPONSE_TYPE;// = "code";
+    //@Value("${auth.base.url}")
+    String AUTH_BASE_URL = "https://auth.globus.org/v2/oauth2";
+    //@Value("$(auth.uri")
+    String AUTH_URI = "/authorize";
+    //@Value("${token.uri}")
+    String TOKEN_URI = "/token";
+    //@Value("${transfer.base.url}")
+    String TRANSFER_BASE_URL = "https://transfer.api.globusonline.org/v0.10";
+    //@Value("$(transfer.uri)")
+    String TRANSFER_URI = "/transfer";
+    //@Value("${submission.uri}")
+    String SUBMISSION_URI = "/submission_id";
+    //@Value("${redirect.uri}")
+    String REDIRECT_URI = "https://127.0.0.1:8080/api/stork/oauth";
+    //@Value("${client.id}")
+    String CLIENT_ID = "6843af68-87f8-4341-bfc4-3db4b1e2d845";
+    //@Value("${client.secret}")
+    String CLIENT_SECRET = "IsqQ6AlvDCi/z5em+D6NJl2cRFq9gFt0jm9GRseQek0=";
+    //@Value("${scope}")
+    String SCOPE = "urn:globus:auth:scope:transfer.api.globus.org:all urn:globus:auth:scope:auth.globus.org:view_identities offline_access";
+    //@Value("$(response.type)")
+    String RESPONSE_TYPE = "code";
     private String accessToken;
 
     public GlobusClient() {
@@ -69,12 +77,20 @@ public class GlobusClient {
     public Mono<String> getAccessToken(String authCode) {
         Map<String, String> authRequestVariables = new HashMap<>();
         authRequestVariables.put("redirect_uri", REDIRECT_URI);
-        authRequestVariables.put("grant_type", authCode);
-        return webClient.get()
-                .uri(TOKEN_URI, authRequestVariables)
+        authRequestVariables.put("grant_type", "authorization_code");
+        authRequestVariables.put("code", authCode);
+        String encode = Base64.getEncoder().encodeToString((CLIENT_ID+":"+CLIENT_SECRET).getBytes());
+
+        return webClient.post()
+                .uri(uriBuilder -> uriBuilder.path(TOKEN_URI)
+                        .queryParam("redirect_uri", REDIRECT_URI)
+                        .queryParam("code", authCode)
+                        .queryParam("grant_type", "authorization_code")
+                        .build())
+                .header("Authorization", "Basic " + encode)
                 .retrieve()
-                .bodyToMono(TokenResponse.class)
-                .map(TokenResponse::getAccessToken);
+                .bodyToMono(CustomTokenResponse.class)
+                .map(CustomTokenResponse::getAccessToken);
     }
 
     public Mono<FileList> list(String path, Boolean showHidden, Integer limit, Integer offset, String orderBy,
