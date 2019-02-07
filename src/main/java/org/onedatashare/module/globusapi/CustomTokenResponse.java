5cc5c1970bc57b60cd1061e0d03af9fdacce7363
yifuBA <yifuyin@buffalo.com>
2019-02-07 12:05:40 -0500

    Changed token customly

--- src/main/java/org/onedatashare/module/globusapi/CustomTokenResponse.java
+++ src/main/java/org/onedatashare/module/globusapi/CustomTokenResponse.java
@@ -5,9 +5,16 @@
 
 package org.onedatashare.module.globusapi;
 
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.json.GenericJson;
 import com.google.api.client.util.Key;
 import com.google.api.client.util.Preconditions;
+import sun.util.calendar.BaseCalendar;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Date;
 
 public class CustomTokenResponse extends GenericJson {
     @Key("access_token")
@@ -16,16 +23,45 @@ public class CustomTokenResponse extends GenericJson {
     private String tokenType;
     @Key("expires_in")
     private Integer expiresInSeconds;
+
+    @Key("resource_server")
+    public String resource_server;
     @Key("refresh_token")
     private String refreshToken;
     @Key
     private String scope;
 
+    @Key("other_tokens")
+    private ArrayList<LinkedHashMap> other_token;
+
+
+    private Calendar recievedTime;
+
     public CustomTokenResponse() {
+        recievedTime = Calendar.getInstance();
+        recievedTime.add(Calendar.SECOND, 172800);
     }
 
     public final String getAccessToken() {
-        return this.accessToken;
+        for(int i = 0; i < other_token.size(); i++){
+            if("transfer.api.globus.org".equals(other_token.get(i).get("resource_server"))){
+                return (String)other_token.get(i).get("access_token");
+            }
+        }
+        return "not found";
+    }
+
+    public final Date getExpiredTime() {
+        for(int i = 0; i < other_token.size(); i++){
+            if("transfer.api.globus.org".equals(other_token.get(i).get("resource_server"))){
+                return recievedTime.getTime();
+            }
+        }
+        return null;
+    }
+
+    public final String getResourceServer(){
+        return this.resource_server;
     }
 
     public CustomTokenResponse setAccessToken(String accessToken) {
